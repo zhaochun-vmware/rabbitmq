ae62b8c5dc682e6a2add94590ddc2b0b1bf252bf
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-09 13:44:40 +0800

    Update 00-cluster-essential.sh

--- tanzu-rabbitmq/00-cluster-essential.sh
+++ tanzu-rabbitmq/00-cluster-essential.sh
@@ -1,7 +1,7 @@
 mkdir $HOME/tanzu-cluster-essentials
-tar -xzf tanzu-cluster-essentials-linux-amd64-1.4.1.tgz -C $HOME/tanzu-cluster-essentials
+tar -xzf tanzu-cluster-essentials-linux-amd64-1.5.0.tgz -C $HOME/tanzu-cluster-essentials
 
-export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:2354688e46d4bb4060f74fca069513c9b42ffa17a0a6d5b0dbb81ed52242ea44
+export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:c7b0988cf3e982625287b241db5d78c30780886dfe9ada01559bb5cd341e6181
 export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
 export INSTALL_REGISTRY_USERNAME=TANZU-NET-USER
 export INSTALL_REGISTRY_PASSWORD=TANZU-NET-PASSWORD
