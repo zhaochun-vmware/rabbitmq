2b5c472a07af4072ed104ed26ef868fbd3ef50ff
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-26 15:35:03 +0800

    Create 6-loal-path-storage.sh

---
+++ install/6-loal-path-storage.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+kubectl annotate storageclass local-path storageclass.kubernetes.io/is-default-class=true
