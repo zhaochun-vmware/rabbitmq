b0c305384f168500d9b1daebc863969d95a653b7
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-30 20:11:00 +0800

    Update 6-local-path-storage.sh

--- install/6-local-path-storage.sh
+++ install/6-local-path-storage.sh
@@ -1,2 +1,5 @@
+kubectl get packages
+
+
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 kubectl annotate storageclass local-path storageclass.kubernetes.io/is-default-class=true
